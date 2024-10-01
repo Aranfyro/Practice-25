d2bf166ac614b3ed1dd6198fbc15bad98377087b
Aranfyro <141035777+Aranfyro@users.noreply.github.com>
2024-10-01 16:06:29 +0300

    Add files via upload

---
+++ module_5_3.py
@@ -0,0 +1,83 @@
+# Домашняя работа по уроку "Перегрузка операторов."
+# Задача "Нужно больше этажей":
+
+class House:
+    def __init__ (self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __len__ (self):
+        return self.number_of_floors
+
+    def __str__ (self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+    def __eq__ (self, other):
+        if self.number_of_floors == other:
+            return True
+        else:
+            return False
+
+    def __lt__ (self, other):
+        if self.number_of_floors < other:
+            return True
+        else:
+            return False
+
+    def __le__ (self, other):
+        if self.number_of_floors <= other:
+            return True
+        else:
+            return False
+
+    def __gt__ (self, other):
+        if self.number_of_floors > other:
+            return True
+        else:
+            return False
+
+    def __ge__ (self, other):
+        if self.number_of_floors >= other:
+            return True
+        else:
+            return False
+
+    def __ne__ (self, other):
+        if self.number_of_floors != other:
+            return True
+        else:
+            return False
+
+    def __add__ (self, value):
+        self.number_of_floors += value
+        return self
+
+    def __radd__ (self, value):
+        return self.__add__(value)
+
+    def __iadd__ (self, value):
+        return self.__add__(value)
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(h1 == h2) # __eq__
+
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
